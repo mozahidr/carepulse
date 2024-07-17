9eb762cbf45cc95af71a70d942b7f2f53cfff127
Mozahid RANA <mozahidpic@gmail.com>
2024-07-17 19:49:43 +1000

    admin passkey verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,41 @@
-import { PatientForm } from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import { PatientForm } from '@/components/forms/PatientForm';
+import PasskeyModal from '@/components/PasskeyModal';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP verification */}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image 
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+          <Link href='/'>
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+          </Link>
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
-           <p className="justify-items-end text-dark-600 xl:text-left">&copy; 2024 CarePulse</p>
-           <Link href='/?admin' className="text-green-500 hover:underline">Admin</Link>
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 CarePulse
+            </p>
+            <Link
+              href="/?admin=true"
+              className="text-green-500 hover:underline"
+            >
+              Admin
+            </Link>
           </div>
         </div>
       </section>
-      <Image 
+      <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
